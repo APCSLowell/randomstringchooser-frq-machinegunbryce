b64b6be018b9b4ceeeb0a88b4c90f0a671f9b7c2
machinegunbryce <brchan1@s.sfusd.edu>
2025-04-26 00:07:19 -0700

    Update RandomLetterChooser.java

--- src/main/java/RandomLetterChooser.java
+++ src/main/java/RandomLetterChooser.java
@@ -1,32 +1,17 @@
-import java.util.*;
-
-public class RandomStringChooser
+public class RandomLetterChooser extends RandomStringChooser
 {
-    private String[] values;
-    private int valuesRemaining;
-
-    public RandomStringChooser(String[] vals)
-    {
-        values = new String[vals.length];
-        for (int i = 0; i < vals.length; i++) {
-            values[i] = vals[i];
-        }
-        valuesRemaining = values.length;
+    public RandomLetterChooser(String str)
+    { 
+        super(getSingleLetters(str));
     }
 
-    public String getNext()
-    {
-        if (valuesRemaining == 0) {
-            return "NONE";
+    private static String[] getSingleLetters(String str)
+    { 
+        String[] letterArray = new String[str.length()];
+        for (int i = 0; i < str.length(); i++)
+        {
+            letterArray[i] = "" + str.charAt(i);
         }
-
-        int index = (int) (Math.random() * valuesRemaining);
-        String selected = values[index];
-
-        // Swap the selected value with the last available one
-        values[index] = values[valuesRemaining - 1];
-        valuesRemaining--;
-
-        return selected;
+        return letterArray;
     }
 }
